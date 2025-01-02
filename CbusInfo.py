b5b39c3af89ce2955cdca932878b1fac6c6c664e
Sven Rosvall <sven@rosvall.ie>
2025-01-02 17:17:14 +0000

    Add script to query node parameters.

--- CbusInfo.py
+++ CbusInfo.py
@@ -23,12 +23,27 @@ def flags(f: int):
                      for bitvalue in [1<<bit for bit in range(8)]
                      if (f & bitvalue) != 0])
 
+def cpuName(manId: int, cpuId: int):
+    if manId == CPUM_MICROCHIP:
+        if cpuId in VlcbMicrochipProcessors:
+            return VlcbMicrochipProcessors[cpuId]
+        else:
+            return f"Unknown Microchip processor(cpuId)"
+    else:
+        return f"Unknown CPU Manufacturer({manId})"
+    
+
 def showCbusMessage(canFrame: canmessage):
     if canFrame.data[0] == OPC_PNN :
         print("PNN NN=", nodeNumber(canFrame.data[1], canFrame.data[2]),
               " Manufacturer=", manufacturerName(canFrame.data[3]),
               " ModuleID=", moduleName(canFrame.data[4]),
               " Flags=", flags(canFrame.data[5]),
+              sep='')
+    elif canFrame.data[0] == OPC_PARAN :
+        print("PARAN NN=", nodeNumber(canFrame.data[1], canFrame.data[2]),
+              " Parameter no: ", canFrame.data[3],
+              " Parameter value=", canFrame.data[4],
               sep='')
     else:
         print("Unsupported OP code:", canFrame.data[0], VlcbOpCodes[canFrame.data[0]])
---
+++ QueryNodeParameters.py
@@ -0,0 +1,57 @@
+# Query nodes on the bus.
+# Send QNN and collect PNN responses.
+
+from sys import argv
+from CbusServerConnection import *
+from CbusInfo import *
+
+def hibyte(n: int) -> int:
+    return n >> 8
+
+def lobyte(n: int) -> int:
+    return n & 0xFF
+
+if len(argv) > 1:
+    nn=int(argv[1])
+else:
+    nn=0
+
+cbusConnection=CbusServerConnection() 
+responses = cbusConnection.askMessage(canmessage(data = [OPC_RQNPN, hibyte(nn), lobyte(nn), 0]))
+
+paramResponses=[]
+for canFrame in responses:
+    if canFrame.get_op_code() == OPC_PARAN :
+        #canFrame.print()
+        # showCbusMessage(canFrame)
+        paramResponses.append(canFrame)
+#print(f"Got {len(paramResponses)} responses.")
+
+if paramResponses[0].data[3] != 0:
+    print("The first response is not the number of parameters")
+    exit(0)
+nParams = paramResponses[0].data[4]
+#print(f"Node has {nParams} parameters")
+
+parameters={}
+if len(paramResponses) == 1:
+    # Need to get the params one by one.
+    #print("will get each param one by one")
+    for i in range(nParams):
+        responses = cbusConnection.askMessage(canmessage(data = [OPC_RQNPN, hibyte(nn), lobyte(nn), i+1]))
+        for canFrame in responses:
+            if canFrame.get_op_code() == OPC_PARAN :
+                #showCbusMessage(canFrame)
+                parameters[canFrame.data[3]] = canFrame.data[4]
+                break
+else:
+    for canFrame in paramResponses[1:]:
+        parameters[canFrame.data[3]] = canFrame.data[4]
+
+#print(f"Got {len(parameters)} parameters")        
+
+print("Module type:", manufacturerName(parameters[PAR_MANU]),
+      moduleName(parameters[PAR_MTYP]),
+      f"{parameters[PAR_MAJVER]}{chr(parameters[PAR_MINVER])}{parameters[PAR_BETA]}")
+print("Module flags:", flags(parameters[PAR_FLAGS]))
+print("CPU:", cpuName(parameters[PAR_CPUMAN], parameters[PAR_CPUID]))
